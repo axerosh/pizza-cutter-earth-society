be1c6a4a6bdc8af4c461dfd7eb9e74e594b5967d
Axel Nordanskog <axeno840@student.liu.se>
2019-05-17 20:31:50 +0200

    Add Spinning prefab

---
+++ Pizza Cutter Earth Society/Assets/Prefabs/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+	private float rotationSpeed = 0.2f * Mathf.PI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
