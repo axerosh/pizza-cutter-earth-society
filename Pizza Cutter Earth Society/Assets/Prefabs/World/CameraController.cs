aee2060d593e67adef58e5e3001f0a18e84bcff8
Axel Nordanskog <axeno840@student.liu.se>
2019-05-18 18:14:37 +0200

    Add camera bounding box

--- Pizza Cutter Earth Society/Assets/Prefabs/World/CameraController.cs
+++ Pizza Cutter Earth Society/Assets/Prefabs/World/CameraController.cs
@@ -4,6 +4,7 @@ public class CameraController : MonoBehaviour
 {
 	public float cameraAcceleration;
 	public float cameraSpeedDecay;
+	public Rect boundingBox;
 
 	private Vector3 cameraMovement;
 
@@ -27,9 +28,35 @@ public class CameraController : MonoBehaviour
 		}
 	}
 
+	void ClampBoundingBox()
+	{
+		Vector3 newLocalPosition = transform.localPosition;
+
+		if (transform.localPosition.x < boundingBox.xMin)
+		{
+			newLocalPosition.x = boundingBox.xMin;
+		}
+		else if (transform.localPosition.x > boundingBox.xMax)
+		{
+			newLocalPosition.x = boundingBox.xMax;
+		}
+
+		if (transform.localPosition.z < boundingBox.yMin)
+		{
+			newLocalPosition.z = boundingBox.yMin;
+		}
+		else if (transform.localPosition.z > boundingBox.yMax)
+		{
+			newLocalPosition.z = boundingBox.yMax;
+		}
+
+		transform.localPosition = newLocalPosition;
+	}
+
 	void UpdatePosition()
 	{
 		transform.localPosition += cameraMovement;
+		ClampBoundingBox();
 
 		//Decay movementspeed.
 		cameraMovement *= cameraSpeedDecay;
