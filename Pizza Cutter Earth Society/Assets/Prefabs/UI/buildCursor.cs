3795eb6048b122b6a969f406d5d743070c382739
Daniel <d.carlsson1@gmail.com>
2019-05-18 16:54:34 +0200

    Tweaked build cursor

---
+++ Pizza Cutter Earth Society/Assets/Prefabs/UI/buildCursor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildCursor : MonoBehaviour {
+
+    public Color disabledColor;
+
+    Color initialColor;
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer> ();
+        initialColor = spriteRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    private void OnTriggerEnter (Collider other) {
+        if (!other.CompareTag ("BuildableArea")) {
+            spriteRenderer.material.color = disabledColor;
+        }
+    }
+
+    private void OnTriggerExit (Collider other) {
+        if (other.CompareTag ("BuildableArea")) {
+            spriteRenderer.material.color = disabledColor;
+        }
+        spriteRenderer.material.color = initialColor;
+    }
+}
