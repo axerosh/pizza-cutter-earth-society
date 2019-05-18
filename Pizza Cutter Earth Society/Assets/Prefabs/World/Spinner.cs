49a71cb212805769c30a38a107b5f34fa7da6b12
Axel Nordanskog <axeno840@student.liu.se>
2019-05-18 15:30:25 +0200

    Split out camera controls from Spinner script

--- Pizza Cutter Earth Society/Assets/Prefabs/World/Spinner.cs
+++ Pizza Cutter Earth Society/Assets/Prefabs/World/Spinner.cs
@@ -1,43 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Spinner : MonoBehaviour
 {
 	public float rotationSpeed = 0.2f;
 
-    public GameObject mainCamera;
-    public float cameraAcceleration;
-    public float cameraSpeedDecay;
-
-    private Vector3 cameraMovement;
-
-    void UpdateInput() {
-        if (Input.GetKey(KeyCode.W)) {
-            cameraMovement.z += cameraAcceleration;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            cameraMovement.x -= cameraAcceleration;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            cameraMovement.z -= cameraAcceleration;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            cameraMovement.x += cameraAcceleration;
-        }
-    }
-
-    void UpdatePosition() {
-        mainCamera.transform.localPosition += cameraMovement;
-
-        //Decay movementspeed.
-        cameraMovement *= cameraSpeedDecay;
-    }
-
     // Update is called once per frame
     void Update() {
-        UpdateInput();
-        UpdatePosition();
 		transform.Rotate(Vector3.up, rotationSpeed * Mathf.PI * Time.deltaTime);
     }
 }
