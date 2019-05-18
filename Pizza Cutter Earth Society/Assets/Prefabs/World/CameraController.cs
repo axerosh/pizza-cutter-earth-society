f21717a271937a13dbec25f90619876b9c166f71
Axel Nordanskog <axeno840@student.liu.se>
2019-05-18 17:22:53 +0200

    Move camera controller component to camera

--- Pizza Cutter Earth Society/Assets/Prefabs/World/CameraController.cs
+++ Pizza Cutter Earth Society/Assets/Prefabs/World/CameraController.cs
@@ -2,7 +2,6 @@
 
 public class CameraController : MonoBehaviour
 {
-	public GameObject mainCamera;
 	public float cameraAcceleration;
 	public float cameraSpeedDecay;
 
@@ -30,7 +29,7 @@ public class CameraController : MonoBehaviour
 
 	void UpdatePosition()
 	{
-		mainCamera.transform.localPosition += cameraMovement;
+		transform.localPosition += cameraMovement;
 
 		//Decay movementspeed.
 		cameraMovement *= cameraSpeedDecay;
