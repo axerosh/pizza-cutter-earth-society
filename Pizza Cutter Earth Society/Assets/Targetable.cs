676ebbb1daa0acb907a5792b4ad7a614f3f4323f
Erik Mansén <erima668@student.liu.se>
2019-05-18 10:55:54 +0200

    Make raycasts detect cutter handle and blade properly

--- Pizza Cutter Earth Society/Assets/Targetable.cs
+++ Pizza Cutter Earth Society/Assets/Targetable.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 public class Targetable : MonoBehaviour {
 
     public Targets targetType;
-    private GameObject targetObject;
+    public GameObject targetObject;
 
-    public GameObject GetObject() {
-        return targetObject;
-    }
-
-    private void Start() {
-        targetObject = gameObject;
-    }
 }
