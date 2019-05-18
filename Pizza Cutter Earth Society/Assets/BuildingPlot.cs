ca36e8ccad511da761cc86912354e0968ebc3e38
Erik Mansén <erima668@student.liu.se>
2019-05-18 12:14:57 +0200

    Implement BuildingPlot for constructing buildings

--- Pizza Cutter Earth Society/Assets/BuildingPlot.cs
+++ Pizza Cutter Earth Society/Assets/BuildingPlot.cs
@@ -4,9 +4,47 @@ using UnityEngine;
 
 public class BuildingPlot : MonoBehaviour {
 
-    public Dictionary<ResourceTypes, int> buildRequirements = new Dictionary<ResourceTypes, int>();
+    public List<ResourceTypes> requiredTypes;
+    public List<int> requiredAmounts;
 
-    public void AddResource(ResourceTypes type, int amount) {
+    public GameObject building; //Finished building.
+    public GameObject plotBox; //Used to display the building site.
 
+    private Dictionary<ResourceTypes, int> buildRequirements = new Dictionary<ResourceTypes, int>();
+    private Dictionary<ResourceTypes, int> gatheredResources = new Dictionary<ResourceTypes, int>();
+
+    private void Start() {
+
+        Debug.Assert(requiredTypes.Count == requiredAmounts.Count, "INCORRECT BUILD REQUIREMENTS");
+
+        for(int i = 0; i < requiredTypes.Count; ++i) {
+            buildRequirements.Add(requiredTypes[i], requiredAmounts[i]);
+            gatheredResources.Add(requiredTypes[i], 0);
+        }
+    }
+
+    //Checks if building complete, if so, activates it and makes the plot invisible.
+    private void CheckCompletion() {
+        foreach(ResourceTypes type in buildRequirements.Keys) {
+            if(gatheredResources[type] < buildRequirements[type]) {
+                return; //Materials still missing.
+            }
+        }
+        building.SetActive(true);
+        plotBox.SetActive(false);
+    }
+
+    /*
+     * Returns actual amount of resources used.
+    */
+    public int AddResource(ResourceTypes type, int amount) {
+        if (buildRequirements.ContainsKey(type)) {
+            //either use all of amount, or as many as are needed to fill the type requirement.
+            int actualAmount = Mathf.Min(amount, (buildRequirements[type] - gatheredResources[type]));
+            gatheredResources[type] += actualAmount;
+            
+            return actualAmount;
+        }
+        return 0;
     }
 }
--- Pizza Cutter Earth Society/Assets/Prefabs/Player/Player.cs
+++ Pizza Cutter Earth Society/Assets/Prefabs/Player/Player.cs
@@ -29,15 +29,12 @@ public class Player : MonoBehaviour {
         }
         if (Input.GetMouseButtonDown(1) && selected.Count > 0) {
             //On a right-click, try to give orders to selected units, if there are any.
-            Debug.Log("Right-click!");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit)) {
-                Debug.Log("Ray hit!");
                 //If a left click hits a unit, select it.
                 Targetable hitTarget = hit.transform.gameObject.GetComponent<Targetable>();
 
                 if (hitTarget) {
-                    Debug.Log("Hit target!");
                     //Pass hit object and hit position to every unit, let them figure out what to do with it.
                     foreach (Unit u in selected) {
                         u.Order(hitTarget, hit.point);
