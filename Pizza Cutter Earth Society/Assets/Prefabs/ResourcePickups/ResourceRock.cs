759244c152ddc40fc9a6840ad6cef2781adb4387
Fendse <46252070+Fendse@users.noreply.github.com>
2019-05-18 17:50:27 +0200

    Rocks now drop resources over time as they're being mined, rather than all at once at the end

--- Pizza Cutter Earth Society/Assets/Prefabs/ResourcePickups/ResourceRock.cs
+++ Pizza Cutter Earth Society/Assets/Prefabs/ResourcePickups/ResourceRock.cs
@@ -7,26 +7,37 @@ using Random = UnityEngine.Random;
 public class ResourceRock : MonoBehaviour
 {
     public int hp;
+    private int currentHp;
 
     public GameObject pickupPrefab;
     public int totalContents;
     public int LARGEST_SINGLE_DROP = 10;
     public int DROP_RANGE = 5;
 
-    public void Damage(int amount) {
-        hp -= amount;
-        Debug.Log("A rock lost " + amount + " health and now has " + hp + " health");
-        if (hp <= 0) {
-            while (totalContents > 0) {
-                int maxDrop = Math.Min(totalContents, LARGEST_SINGLE_DROP);
-                int dropSize = UnityEngine.Random.Range(1, maxDrop);
-                totalContents -= dropSize;
+    void Start() {
+        currentHp = hp;
+    }
 
-                GameObject newDrop = Instantiate(pickupPrefab);
-                newDrop.GetComponent<ResourcePickup>().resourceQuantity = dropSize;
-                Vector2 rand = Random.insideUnitCircle * DROP_RANGE;
-                newDrop.transform.position = transform.position + new Vector3(rand.x, 0, rand.y);
-            }
+    public void Damage(int amount) {
+        Debug.Log("A rock lost " + amount + " health and now has " + (currentHp - amount) + " health");
+        int hpBefore = currentHp;
+        int hpAfter = currentHp - amount;
+
+        int hpPerResource = (int) Math.Ceiling((float) hp / totalContents);
+
+        int resourcesDropped = (hpBefore / hpPerResource) - (hpAfter / hpPerResource);
+
+        if (resourcesDropped > 0) {
+            Debug.Log("A rock dropped " + resourcesDropped + " resources");
+            GameObject newDrop = Instantiate(pickupPrefab);
+            newDrop.GetComponent<ResourcePickup>().resourceQuantity = resourcesDropped;
+            Vector2 rand = Random.insideUnitCircle * DROP_RANGE;
+            newDrop.transform.position = transform.position + new Vector3(rand.x, 0, rand.y);
+        }
+        
+        currentHp = hpAfter;
+        
+        if (currentHp <= 0) {
             Destroy(gameObject);
         }
     }
