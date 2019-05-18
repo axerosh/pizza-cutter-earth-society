706e874fc3a1aa2c15ce408dcab068b81e147c96
Erik Mansén <erima668@student.liu.se>
2019-05-18 14:28:53 +0200

    Moved around a few files, implement IDeliver interface for resource dropof points

---
+++ Pizza Cutter Earth Society/Assets/Prefabs/Buildings/IDeliver.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDeliver {
+    int Deliver(ResourceTypes type, int amount);
+
+    bool RequiresResource(ResourceTypes? type);
+}
