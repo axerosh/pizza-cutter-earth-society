0627f3fa4c7a8d75f162ed8c0f8be26650139384
Daniel <d.carlsson1@gmail.com>
2019-05-18 11:34:39 +0200

    Initial commit of resources UI

---
+++ Pizza Cutter Earth Society/Assets/UIController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    public GameObject unitUIPrefab;
+    public GameObject buildPlotUI;
+
+    UnitUI selectedUnit;
+
+    Transform canvasTransform;
+
+    public Unit testUnit;
+
+    void Start () {
+        canvasTransform = transform.Find ("Canvas");
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown (0)) {
+            SelectUnit (testUnit);
+        }
+        if (Input.GetMouseButtonDown (1)) {
+            DeselectUnit ();
+        }
+    }
+
+    public void SelectUnit (Unit unit) {
+        if (selectedUnit != null) {
+            DeselectUnit ();
+        }
+
+        GameObject unitGO = Instantiate (unitUIPrefab, Camera.main.WorldToScreenPoint (unit.transform.position), Quaternion.identity, canvasTransform);
+        unitGO.name = "SelectedUnitUI";
+        selectedUnit = unitGO.GetComponent<UnitUI> ();
+        selectedUnit.Init (unit);
+    }
+
+    public void DeselectUnit () {
+        if (selectedUnit != null) {
+            Destroy (selectedUnit.gameObject);
+            selectedUnit = null;
+        }
+    }
+
+    public void ShowBuildMenu () {
+
+    }
+
+    public void HideBuildMenu () {
+
+    }
+}
---
+++ Pizza Cutter Earth Society/Assets/UnitUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitUI : MonoBehaviour {
+    public Unit unit;
+    public GameObject resourcePrefab;
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (unit != null) {
+            transform.position = Camera.main.WorldToScreenPoint (unit.transform.position);
+
+            //TODO: Update Unit resources
+        }
+    }
+
+    public void Init (Unit unit) {
+        this.unit = unit;
+
+        //TODO: Init resources
+
+    }
+}
