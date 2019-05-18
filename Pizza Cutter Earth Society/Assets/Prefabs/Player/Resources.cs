aff27dffd9828033c97a3c3c6f48601884f6b863
Fendse <46252070+Fendse@users.noreply.github.com>
2019-05-18 13:39:54 +0200

    Attempting to associate pickups w/ resource types

--- Pizza Cutter Earth Society/Assets/Prefabs/Player/Resources.cs
+++ Pizza Cutter Earth Society/Assets/Prefabs/Player/Resources.cs
@@ -6,57 +6,9 @@ using UnityEngine.Assertions;
 
 public enum ResourceTypes { VESUVIUM, HAWAIIUM, FUNGHITE, SALAMITE };
 
+
 public class Resources : MonoBehaviour
 {
-    public Dictionary<ResourceTypes, int> resources;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        resources = new Dictionary<ResourceTypes, int>();
-        foreach (ResourceTypes type in (ResourceTypes[])Enum.GetValues(typeof(ResourceTypes))) {
-            resources.Add(type, 0);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    bool CanSpend(Dictionary<ResourceTypes, int> amounts) {
-        foreach (KeyValuePair<ResourceTypes, int> kv in amounts) {
-            if (resources[kv.Key] < kv.Value) return false;
-        }
-
-        return true;
-    }
-
-    bool Spend(Dictionary<ResourceTypes, int> amounts) {
-        if (!CanSpend(amounts)) return false;
-
-        foreach (KeyValuePair<ResourceTypes, int> kv in amounts) {
-            resources[kv.Key] -= kv.Value;
-        }
-
-        return true;
-    }
-
-    bool Spend(ResourceTypes type, int amount) {
-        var dict = new Dictionary<ResourceTypes, int>();
-        dict.Add(type, amount);
-        return Spend(dict);
-    }
-
-    void Gain(Dictionary<ResourceTypes, int> amounts) {
-
-        foreach (KeyValuePair<ResourceTypes, int> kv in amounts) {
-            resources[kv.Key] += kv.Value;
-        }
-    }
-
-    void Gain(ResourceTypes type, int amount) {
-        resources[type] += amount;
-    }
+    public List<GameObject> pickups;
 }
