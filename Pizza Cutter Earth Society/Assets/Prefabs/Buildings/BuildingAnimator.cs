224d24b126d7a1d14b220f6198558c37ef71e84a
Daniel <d.carlsson1@gmail.com>
2019-05-18 14:11:00 +0200

    Added building animation

---
+++ Pizza Cutter Earth Society/Assets/Prefabs/Buildings/BuildingAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingAnimator : MonoBehaviour {
+
+    public float scaleSpeed = 5;
+    Vector3 initialScale;
+
+    void OnEnable () {
+        initialScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+        StartCoroutine (ScaleUpAsync ());
+    }
+
+    void OnDisable () {
+        StopCoroutine ("ScaleUpAsync");
+    }
+
+    IEnumerator ScaleUpAsync () {
+        while (transform.localScale != initialScale) {
+            transform.localScale = Vector3.MoveTowards (transform.localScale, initialScale, Time.deltaTime * scaleSpeed);
+            yield return null;
+        }
+        yield return null;
+    }
+}
