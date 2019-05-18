9f9a8605645007fce8864f98bc94726e99eca679
Axel Nordanskog <axeno840@student.liu.se>
2019-05-18 15:49:59 +0200

    Add pizza wall

---
+++ Pizza Cutter Earth Society/Assets/Prefabs/World/TextureScroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroller : MonoBehaviour
+{
+	public List<Vector2> scrollSpeeds = new List<Vector2>();
+
+	new Renderer renderer;
+
+	void Start()
+	{
+		renderer = GetComponentInChildren<Renderer>();
+	}
+	
+	void Update()
+	{
+		for (int i = 0; i < renderer.materials.Length; ++i)
+		{
+			Material mat = renderer.materials[i];
+			if (i < scrollSpeeds.Count)
+			{
+				mat.SetTextureOffset("_MainTex", Time.time * scrollSpeeds[i]);
+			}
+		}
+	}
+}
