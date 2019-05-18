68ae8a0b63903ebdcf5a2fa5c8ab2a8c834c4a4b
Daniel <d.carlsson1@gmail.com>
2019-05-18 19:48:59 +0200

    Added building plot UI buttons (Not working yet)

---
+++ Pizza Cutter Earth Society/Assets/BuildButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildButton : MonoBehaviour {
+    public enum Type {
+        Rocket,
+        Capricosium,
+        Kebabite
+    }
+    public Type type;
+
+    public Button button;
+
+    public GameObject resourcePrefab;
+
+    public void Init (Type type) {
+        this.type = type;
+
+        button = GetComponent<Button> ();
+        var text = transform.GetComponentInChildren<TextMeshProUGUI> ();
+        if (text) {
+            text.text = type.ToString ();
+        }
+
+        UpdateResources ();
+    }
+
+    public void UpdateResources () {
+        //TODO: update resources
+    }
+}
